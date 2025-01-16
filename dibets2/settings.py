0d8a16dcad0574e13fcedae764726b2f2a113838
Anasmajestic <130275740+Anasmajestic@users.noreply.github.com>
2025-01-16 14:08:28 +0530

    Update settings.py

--- dibets2/settings.py
+++ dibets2/settings.py
@@ -27,9 +27,9 @@ STATIC_DIR=BASE_DIR.joinpath('static')
 SECRET_KEY = 'django-insecure-^-9cf=8t2dyx-(ts_hcg3x&@2f_-mj_(!&y$b^st4zok-*p+5&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = ['43.255.154.41','diabetesdigest.co','www.diabetesdigest.co']
 
 # Application definition
